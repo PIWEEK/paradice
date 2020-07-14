10763d9afab192bf74c2be4b8bd690aea66d4eda
elhombretecla <delacruzgarciajuan@gmail.com>
2020-07-14 20:06:05 +0200

    Add images and home styles

--- client/src/routes/Home.svelte
+++ client/src/routes/Home.svelte
@@ -1,6 +1,42 @@
 <script>
-import {link} from 'svelte-spa-router'
+  import {link} from 'svelte-spa-router'
 </script>
 
-<h1>HOME</h1>
-<a href="/game/paradice-testing-game" use:link>The test game</a>
+
+<style>
+
+  .home-section {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    max-width: 900px;
+    margin-top: 5vh;
+    padding: 2rem;
+  }
+
+  .big-logo {
+    font-size: .1px;
+  }
+
+  .big-logo img {
+    width: 100%;    
+  }
+
+  .btn-primary {
+    margin-top: 5vh;
+    font-size: 24px;
+  }
+
+</style>
+
+<div class="home-section">
+
+  <h1 class="big-logo">
+    <img src="/images/logo.png" alt="PARADICE" border="0">
+    PARADICE
+  </h1>
+
+  <a class="btn-primary" href="/game/paradice-testing-game" use:link>Start Paradice</a>
+
+</div>
