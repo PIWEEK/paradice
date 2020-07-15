3bd9ad7285056a05f43ea1779a84c1beeed7eae6
elhombretecla <delacruzgarciajuan@gmail.com>
2020-07-15 23:29:53 +0200

    add result banner styles

--- client/src/ResultBanner.svelte
+++ client/src/ResultBanner.svelte
@@ -1,8 +1,32 @@
 <script>
-export let playername = '';
-export let latestroll = 0;
+  export let playername = '';
+  export let latestroll = 0;
 </script>
-<p>Soy ResultBanner</p>
+
+<style>
+
+.roll-result {
+  background-color: rgba(55, 27, 62, 0.8);
+  padding: 0.6rem 1rem;
+  color: #CF4AD9;
+  position: fixed;
+  font-size: 1.6rem;
+  left: 46%;
+  top: 1.6rem;
+  z-index: 1000;
+}
+
+.roll-result span {
+  color: #ffffff;
+  margin-left: .6rem;
+}
+
+.roll-result span.maths {
+  color: rgba(255,255,255,.5);
+}
+
+</style>
+
 {#if playername!=''}
-<p>Player {playername} got {latestroll}</p>
+  <div class="roll-result">{playername} got <span>{latestroll}</span> <span class="maths">1d6 + 2d10 + 6</span></div>
 {/if}
