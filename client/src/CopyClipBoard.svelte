6132f11aea9c9b7acb10f877c874672a9eb0e9d1
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2020-07-16 20:14:36 +0200

    Share game

---
+++ client/src/CopyClipBoard.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { onMount } from 'svelte';
+
+	export let shareUrl;
+
+	let textarea;
+
+	onMount(() => {
+		textarea.select();
+		document.execCommand('copy');
+	});
+</script>
+
+<textarea bind:value={shareUrl} bind:this={textarea}></textarea>
+
