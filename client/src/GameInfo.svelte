fd78a88f9e657760d11982839d51186173ee989a
elhombretecla <delacruzgarciajuan@gmail.com>
2020-07-16 16:45:47 +0200

    add game name styles

--- client/src/GameInfo.svelte
+++ client/src/GameInfo.svelte
@@ -1,1 +1,40 @@
-<h1>Soy Game Info</h1>
+<style>
+
+  h2 {
+    font-size: 1.2rem;
+    margin: 0;
+    white-space: nowrap;
+  }
+
+  .row-flex {
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: .5rem;
+    width: 100%;
+  }
+
+  .copy-link {
+    background-color: #371B3E;
+    cursor: pointer;
+    display: flex;
+    height: 16px;
+    padding: .2rem .3rem;
+    width: 16px;
+  }
+
+  .copy-link:hover {
+    background-color: #CF4AD9;
+  }
+
+  .copy-link img {
+    width: 100%;
+  }
+
+</style>
+
+<div class="row-flex">
+  <h2>TABLE_NAME</h2>
+  <span class="copy-link">
+    <img src="/images/share-table.png" alt="SHARE TABLE">
+  </span>
+</div>
