30184846c2d4531134b1946ccc120e7e85466ecf
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2020-07-16 10:45:23 +0200

    Fixing D20

--- client/src/dice.js
+++ client/src/dice.js
@@ -583,31 +583,28 @@ export class DiceD20 extends DiceObject {
   constructor(options) {
     super(options);
 
-    this.tab = 0;
-    this.af = Math.PI * 6 / 5;
-    this.chamfer = 0.945;
-    this.vertices = [];
-    this.faces = [[5, 7, 11, 0], [4, 2, 10, 1], [1, 3, 11, 2], [0, 8, 10, 3], [7, 9, 11, 4],
-    [8, 6, 10, 5], [9, 1, 11, 6], [2, 0, 10, 7], [3, 5, 11, 8], [6, 4, 10, 9],
-    [1, 0, 2, -1], [1, 2, 3, -1], [3, 2, 4, -1], [3, 4, 5, -1], [5, 4, 6, -1],
-    [5, 6, 7, -1], [7, 6, 8, -1], [7, 8, 9, -1], [9, 8, 0, -1], [9, 0, 1, -1]];
+    let t = (1 + Math.sqrt(5)) / 2;
 
-    for (let i = 0, b = 0; i < 10; ++i, b += Math.PI * 2 / 10) {
-      this.vertices.push([Math.cos(b), Math.sin(b), 0.105 * (i % 2 ? 1 : -1)]);
-    }
-    this.vertices.push([0, 0, -1]);
-    this.vertices.push([0, 0, 1]);
-
-    this.scaleFactor = 0.9;
-    this.values = 10;
+    this.tab = -0.2;
+    this.af = -Math.PI / 4 / 2;
+    this.chamfer = 0.955;
+    this.vertices = [[-1, t, 0], [1, t, 0], [-1, -t, 0], [1, -t, 0],
+    [0, -1, t], [0, 1, t], [0, -1, -t], [0, 1, -t],
+    [t, 0, -1], [t, 0, 1], [-t, 0, -1], [-t, 0, 1]];
+    this.faces = [[0, 11, 5, 1], [0, 5, 1, 2], [0, 1, 7, 3], [0, 7, 10, 4], [0, 10, 11, 5],
+    [1, 5, 9, 6], [5, 11, 4, 7], [11, 10, 2, 8], [10, 7, 6, 9], [7, 1, 8, 10],
+    [3, 9, 4, 11], [3, 4, 2, 12], [3, 2, 6, 13], [3, 6, 8, 14], [3, 8, 9, 15],
+    [4, 9, 5, 16], [2, 4, 11, 17], [6, 2, 10, 18], [8, 6, 7, 19], [9, 8, 1, 20]];
+    this.scaleFactor = 1;
+    this.values = 20;
     this.faceTexts = [' ', '0', '1', '2', '3', '4', '5', '6', '7', '8',
         '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
     this.textMargin = 1.0;
-    this.mass = 350;
-    this.inertia = 9;
+    this.mass = 400;
+    this.inertia = 6;
 
     this.create();
-}
+  }
 }
 
 export class DiceD100 extends DiceObject {
