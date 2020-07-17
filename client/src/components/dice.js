bb94cfd10236994d2ed4c4c10ea8dae5ca7b2472
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2020-07-17 10:16:44 +0200

    D10 faces

--- client/src/components/dice.js
+++ client/src/components/dice.js
@@ -544,7 +544,7 @@ export class DiceD10 extends DiceObject {
         this.scaleFactor = 0.9;
         this.values = 10;
         this.faceTexts = [' ', '0', '1', '2', '3', '4', '5', '6', '7', '8',
-            '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
+            '9', '0', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
         this.textMargin = 1.0;
         this.mass = 350;
         this.inertia = 9;
